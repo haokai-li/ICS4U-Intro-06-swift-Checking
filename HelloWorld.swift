c7f70b26f18b5cc9f6f67c40db5b5febbc66125d
Ubuntu <ubuntu@ip-172-31-82-118.ec2.internal>
2021-11-18 18:38:54 +0000

    hello world

---
+++ HelloWorld.swift
@@ -0,0 +1,12 @@
+/*
+* The "Hello Word!" program, just proves you can show print to terminal.
+*
+* @author  Haokai
+* @version 1.0
+* @since   2021-11-17
+*/
+
+// print out "Hello, World!"
+print("Hello, World!")
+
+print("\nDone.")
